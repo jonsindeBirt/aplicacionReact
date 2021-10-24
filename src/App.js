02732713a62c06084eaef9cf034c9c844804f437
jonsinmar <sinde.jon@gmail.com>
2021-10-24 10:53:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cabecera from "./Cabecera";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CabeceraConNombre nombre="Nora" />
+      <Cuerpo />
+      <Footer />
+    </>
+  );
+}
+
+function Cuerpo() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+      vestibulum est id nulla malesuada, sit amet placerat velit dapibus.
+      Suspendisse ac risus ante. Aliquam semper felis ac aliquet eleifend.
+      Aenean a rutrum ipsum. Aenean augue orci, sagittis cursus mauris eu,
+      venenatis sodales turpis. Cras vitae congue ligula. Nunc tincidunt est
+      vitae euismod tempor. Mauris in enim vestibulum, ultrices purus sit amet,
+      porttitor arcu. Vestibulum pretium nibh et porttitor volutpat. Quisque
+      posuere quam sed urna vestibulum, non pretium nisi faucibus. Ut nec tempor
+      enim. Nullam id commodo lacus. Class aptent taciti sociosqu ad litora
+      torquent per conubia nostra, per inceptos himenaeos. Nam vel mauris eget
+      lacus auctor interdum. Sed pharetra aliquet mattis. Praesent interdum
+      malesuada erat, vitae accumsan sapien congue ac. Suspendisse id ultrices
+      neque, vel imperdiet lorem. Duis vitae elit cursus, varius velit eu,
+      dignissim urna. Sed urna lacus, ultrices posuere lectus eu, laoreet
+      maximus eros. Nunc in consequat sapien, a commodo purus. Nullam consequat
+      cursus enim, eleifend aliquet odio ultrices non. Etiam augue nisi, dapibus
+      pretium ipsum ac, congue iaculis justo. Mauris facilisis nibh blandit
+      sagittis efficitur.
+    </p>
+  );
+}
+
+function Footer() {
+  return <a href="https://www.birt.eus/">Birt</a>;
+}
+
+function CabeceraConNombre(propiedades) {
+  return (
+    <h1>
+      Hola {propiedades.nombre} {propiedades.nombre2}
+    </h1>
   );
 }
 
---
+++ src/Cabecera.js
@@ -0,0 +1,5 @@
+function Cabecera() {
+  return <h1>Esto es una aplicación de prueba</h1>;
+}
+
+export default Cabecera;
